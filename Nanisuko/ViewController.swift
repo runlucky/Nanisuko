e0f0b3e43521d97e808d8e12be37d88269cfcbeb
Kakeru Fukuda <stand.by.we@gmail.com>
2019-06-15 14:05:24 +0900

    ガワ作成

---
+++ Nanisuko/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Nanisuko
+//
+//  Created by 福田走 on 2019/06/15.
+//  Copyright © 2019 福田走. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
