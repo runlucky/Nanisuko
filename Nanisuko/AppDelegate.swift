8eaebb1da333a57001136f21fe883356d1b398ed
Kakeru Fukuda <stand.by.we@gmail.com>
2019-09-12 22:30:54 +0900

    #3 通知が消えない問題を修正

--- Nanisuko/AppDelegate.swift
+++ Nanisuko/AppDelegate.swift
@@ -48,11 +48,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
     
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-        UIApplication.shared.applicationIconBadgeNumber = 0
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        UIApplication.shared.applicationIconBadgeNumber = 0
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
